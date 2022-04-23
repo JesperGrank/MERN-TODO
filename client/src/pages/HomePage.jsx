a13448a3ea3d00f707479c354b2fb838219e03aa
Jesper Grankvist <jesper.grankvist@hotmail.com>
2022-04-23 23:44:14 +0200

    Logout function to remove localstorage token

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -2,12 +2,20 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 export default function Register() {
+
+  function logout() {
+    localStorage.removeItem("MERNTODO")
+    console.log("removed token")
+  }
+  
   return (
     <div>
         <h2>HomePage / StartPage</h2>
+        <button onClick={logout}>Logout</button>
+        <br></br>
         <Link to="/register">Register</Link>
         <br></br>
-        <Link to="/login">Login</Link> 
+        <Link to="/login">Login</Link>
     </div>
   )
 }
